69295c849d6578758543763881695595b6dbc971
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-05-08 11:34:32 -0300

    Add new

---
+++ servidor.py
@@ -0,0 +1,36 @@
+"""
+import Pyro4
+
+@Pyro4.expose
+
+class Saludo:
+    def saludar(self):
+        return "Hello"
+
+
+daemon=Pyro4.Daemon()
+
+uri=daemon.register(Saludo)
+ns=Pyro4.locateNS()
+ns.register('obj',uri)
+
+print(uri)
+
+daemon.requestLoop()
+"""
+# saved as greeting-server.py
+import Pyro4
+
+@Pyro4.expose
+class GreetingMaker(object):
+    def get_fortune(self, name):
+        return "Hello, {0}. Here is your fortune message:\n" \
+               "Tomorrow's lucky number is 12345678.".format(name)
+
+daemon = Pyro4.Daemon()                # make a Pyro daemon
+ns = Pyro4.locateNS()                  # find the name server
+uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
+ns.register("example.greeting", uri)   # register the object with a name in the name server
+
+print("Ready.")
+daemon.requestLoop()                   # start the event loop of the server to wait for calls
